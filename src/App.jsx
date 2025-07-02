327a591a517a8281a0c290d90723f00ac65f5850
nurealammiaji <nurealam.miaji@gmail.com>
2025-07-03 02:53:49 +0600

    layout created

--- src/App.jsx
+++ src/App.jsx
@@ -5,47 +5,51 @@ const App = () => {
 
   return (
     <>
-      <div className="drawer">
-        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col">
-          {/* Navbar */}
-          <div className="navbar bg-base-300 w-full">
-            <div className="flex-none lg:hidden">
-              <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="inline-block h-6 w-6 stroke-current"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  ></path>
-                </svg>
-              </label>
+      <div>
+        <div>
+          <div className="relative drawer lg:drawer-open">
+            <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
+            <div className="flex flex-col drawer-content">
+              {/* Navbar */}
+              <div className="w-full navbar bg-base-300">
+                <div className="flex-none lg:hidden">
+                  <label htmlFor="dashboard-drawer" aria-label="open sidebar" className="btn btn-square btn-ghost">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      className="inline-block w-6 h-6 stroke-current">
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h16M4 18h16"></path>
+                    </svg>
+                  </label>
+                </div>
+                <div className="flex-1 px-2 mx-2">
+                   <h3 className='md:text-2xl md:font-bold font-semibold'>Smart Hostel</h3>
+                </div>
+                <div className="flex-none">
+                  <ul className="menu menu-horizontal flex gap-3">
+                    {/* Navbar menu content here */}
+                    <li className='btn btn-sm'>Link-1</li>
+                    <li className='btn btn-sm'>Link-2</li>
+                  </ul>
+                </div>
+              </div>
+              {/* Page content here */}
+              <div className="p-5">
+                <Outlet />
+              </div>
             </div>
-            <div className="mx-2 flex-1 px-2">Navbar Title</div>
-            <div className="flex-none">
-              <ul className="menu menu-horizontal">
-                {/* Navbar menu content here */}
-                <li><a>Navbar Item 1</a></li>
-                <li><a>Navbar Item 2</a></li>
+            <div className="drawer-side">
+              <label htmlFor="dashboard-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+              <ul className="min-h-full p-4 menu bg-base-200 w-60 md:w-80">
+                {/* Sidebar content here */}
               </ul>
             </div>
           </div>
-          {/* Page content here */}
-          <Outlet />
-        </div>
-        <div className="drawer-side">
-          <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
-          <ul className="menu bg-base-200 min-h-full w-80 p-4">
-            {/* Sidebar content here */}
-            <li><a>Sidebar Item 1</a></li>
-            <li><a>Sidebar Item 2</a></li>
-          </ul>
         </div>
       </div>
     </>
